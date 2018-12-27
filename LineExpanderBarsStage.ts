671ac6e2ec1edcdf18bd9dfed6ba0d9aaa017dab
Anwesh43 <anweshthecool0@gmail.com>
2018-12-27 23:41:18 +0530

    adding drawLebnODE method

--- LineExpanderBarsStage.ts
+++ LineExpanderBarsStage.ts
@@ -6,6 +6,7 @@ const sizeFactor : number = 2.7
 const strokeFactor : number = 90
 const nodes : number = 5
 const lines : number = 4
+const color : string = '#283593'
 
 const maxScale : Function = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
 const divideScale : Function = (scale : number, i : number, n : number) : number => Math.min(1/n, maxScale(scale, i, n)) * n
@@ -16,6 +17,41 @@ const mirrorValue : Function = (scale : number, a : number, b : number) : number
 }
 const updateScale : Function = (scale : number, dir : number, a : number, b : number) : number => {
     return mirrorValue(scale, a, b) * dir * scGap
+}
+
+const drawLEBNode : Function = (context : CanvasRenderingContext2D, i : number, scale : number) => {
+    const gap : number = w / (nodes + 1)
+    const size : number = gap / sizeFactor
+    context.lineWidth = Math.min(w, h) / strokeFactor
+    context.lineCap = 'round'
+    context.strokeStyle = color
+    const sc1 : number = divideScale(scale, 0, 2)
+    const sc2 : number = divideScale(scale, 1, 2)
+    const xGap : number = (size) / (lines + 1)
+    context.save()
+    context.translate(gap * (i + 1), h/2)
+    context.rotate(Math.PI/2 * sc1)
+    for (var j = 0; j < lines; j++) {
+        const sc = divideScale(sc1, j, lines)
+        context.save()
+        const jGap : number = (lines - j) * xGap
+        for (var k = 0; k < 2; k++) {
+            const x : number = jGap * (1 - 2 * (k % 2)) * sc
+            context.beginPath()
+            context.moveTo(0,0)
+            context.lineTo(0, x)
+            context.stroke()
+            context.save()
+            context.translate(x, 0)
+            context.beginPath()
+            context.moveTo(0, 0)
+            context.lineTo(0, -xGap)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+    context.restore()
 }
 
 class LineExpanderBarsStage {
