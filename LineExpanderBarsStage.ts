4feca850aa69c7107b5dc732fe9730235448b939
Anwesh43 <anweshthecool0@gmail.com>
2018-12-27 22:38:47 +0530

    adding scale functions

--- LineExpanderBarsStage.ts
+++ LineExpanderBarsStage.ts
@@ -6,6 +6,18 @@ const sizeFactor : number = 2.7
 const strokeFactor : number = 90
 const nodes : number = 5
 const lines : number = 4
+
+const maxScale : Function = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
+const divideScale : Function = (scale : number, i : number, n : number) : number => Math.min(1/n, maxScale(scale, i, n)) * n
+const scaleFactor : Function = (scale : number) : number => Math.floor(scale / scDiv)
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k : number = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+const updateScale : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
+
 class LineExpanderBarsStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
